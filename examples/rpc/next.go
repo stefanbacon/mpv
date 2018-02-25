ab7481bcacbcafb3a1ee6b516085205dc053e8da
Arseny Zorin <zorinarseny@yandex.ru>
2018-02-25 23:41:55 +0300

    Track name

--- examples/rpc/next.go
+++ examples/rpc/next.go
@@ -1,9 +1,26 @@
 package main
 
-import "github.com/ArsenyZorin/mpv"
+import (
+	"github.com/ArsenyZorin/mpv"
+	"os/exec"
+	"strings"
+)
 
 func main() {
+	path := "https://www.youtube.com/watch?v="
 	ipcc := mpv.NewIPCClient("/tmp/mpvsocket")
 	c := mpv.NewClient(ipcc)
 	c.PlaylistNext()
+
+	file,_ := c.Filename()
+	name := file[1 : len(file) - 1]
+
+	cmd := exec.Command("youtube-dl", path + name, "--get-title")
+	out, _ := cmd.CombinedOutput()
+	cmd.Run()
+
+	str := strings.Replace(string(out), "\n", "", -1)
+
+	cmd = exec.Command("dunstify", str, "-r", "3040", "-u", "urgency")
+	cmd.Run()
 }
