40fce7bfd44bf2ce43ab3b09d3228f2a4942088c
Arseny Zorin <zorinarseny@yandex.ru>
2018-02-25 20:20:42 +0300

    Fix get pause state

--- examples/rpc/toggle.go
+++ examples/rpc/toggle.go
@@ -1,10 +1,10 @@
 package main
 
-import "github.com/blang/mpv"
+import "github.com/ArsenyZorin/mpv"
 
 func main() {
 	ipcc := mpv.NewIPCClient("/tmp/mpvsocket")
 	c := mpv.NewClient(ipcc)
-	pause,_ = c.Pause()
+	pause,_ := c.Pause()
 	c.SetPause(!pause)
 }
